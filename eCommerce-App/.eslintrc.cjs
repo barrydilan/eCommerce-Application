c63f3b872b4d3349a53ae1876f7631e92ef364fe
Tedzury <asdrogachev@gmail.com>
2023-08-03 14:29:19 +0300

    feat: init and setup prettier, integration with linter

--- eCommerce-App/.eslintrc.cjs
+++ eCommerce-App/.eslintrc.cjs
@@ -2,11 +2,13 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'plugin:react-hooks/recommended',
     'airbnb', 
     'airbnb-typescript',
     'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
@@ -19,19 +21,19 @@ module.exports = {
     'ecmaVersion': 2021,
     'sourceType': 'module',
   },
-  plugins: ['react', 'import', 'react-refresh', '@typescript-eslint'],
+  plugins: ['react', 'import', 'react-refresh', '@typescript-eslint', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     'react/react-in-jsx-scope': 0,
-    "import/extensions": "off",
+    'import/extensions': 0,
   },
-  "settings": {
-    "import/resolver": {
-        "typescript": {
-            "alwaysTryTypes": true
+  'settings': {
+    'import/resolver': {
+        'typescript': {
+            'alwaysTryTypes': true
         }
     }
   }
