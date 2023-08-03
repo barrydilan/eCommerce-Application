b083c838c1433950c3e43c9843e125cb717b7006
Tedzury <asdrogachev@gmail.com>
2023-08-03 14:11:10 +0300

    fix: fix import issue with tsx file, add eslint import resolver typescipt, remove redundant code and files from app.tsx

--- eCommerce-App/.eslintrc.cjs
+++ eCommerce-App/.eslintrc.cjs
@@ -2,24 +2,37 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'plugin:react-hooks/recommended',
     'airbnb', 
     'airbnb-typescript',
     'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    'project': ['tsconfig.json'],
+    'tsconfigRootDir': __dirname,
     'ecmaVersion': 2021,
     'sourceType': 'module',
-    project: './tsconfig.json'
   },
-  plugins: ['react-refresh'],
+  plugins: ['react', 'import', 'react-refresh', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 0,
+    "import/extensions": "off",
   },
+  "settings": {
+    "import/resolver": {
+        "typescript": {
+            "alwaysTryTypes": true
+        }
+    }
+  }
 }
