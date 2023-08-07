25281010c2907acfe934b833b3060a0de5fb39b0
barrydillan <queharambe@gmail.com>
2023-08-07 23:09:20 +0200

    feat: add signup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,10 @@
 import SignIn from '../widgets/signIn/SignIn';
+import SignUp from '../widgets/signUp/SignUp';
 
 export default function App() {
   return (
     <div>
-      <SignIn />
+      <SignUp />
     </div>
   );
 }
