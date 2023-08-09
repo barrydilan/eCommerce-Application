4954162c5a606583c81408852ba970ebca35f0e1
barrydillan <queharambe@gmail.com>
2023-08-09 12:44:36 +0200

    feat: clear app folder

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,1 @@
-import SignIn from '../widgets/signIn/SignIn';
-import SignUp from '../widgets/signUp/SignUp';
-
-export default function App() {
-  return (
-    <div>
-      <SignUp />
-    </div>
-  );
-}
+export default function App() {}
