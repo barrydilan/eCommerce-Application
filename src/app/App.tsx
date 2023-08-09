eece4089fb0ce4b9a0d6a0d140a07bb9a56a659f
Bogdan <thegarrybit@gmail.com>
2023-08-09 14:51:25 +0300

    refactor: resolve merge conflict

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,1 +1,17 @@
-export default function App() {}
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<p>Main page</p>} />
+        <Route path="registration" element={<p>Registration</p>} />
+        <Route path="login" element={<p>Login</p>} />
+        <Route path="delivery" element={<p>Delivery</p>} />
+        <Route path="payment" element={<p>Payment</p>} />
+        <Route path="about" element={<p>About us</p>} />
+        <Route path="*" element={<p>Page not found</p>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
