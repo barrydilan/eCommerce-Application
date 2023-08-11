e2bec59019fe227c1f7b4c8460341f9c0d6cb346
Bogdan <thegarrybit@gmail.com>
2023-08-11 18:30:00 +0300

    refactor: move all constants into separate file

---
+++ src/shared/model/constants.ts
@@ -0,0 +1,5 @@
+export const PROJECT_KEY = 'async-await-ecommerce-application';
+export const CLIENT_ID = 'placeholder';
+export const CLIENT_SECRET = 'placeholder';
+export const AUTH_SERVICE_URL = `https://auth.europe-west1.gcp.commercetools.com`;
+export const DEFAULT_CUSTOMER_SCOPE = `view_published_products:${PROJECT_KEY} manage_my_orders:${PROJECT_KEY} manage_my_profile:${PROJECT_KEY}`;
--- src/shared/model/index.ts
+++ src/shared/model/index.ts
@@ -1,1 +1,2 @@
 export { useAppDispatch, useAppSelector } from './hooks.ts';
+export { PROJECT_KEY, DEFAULT_CUSTOMER_SCOPE, AUTH_SERVICE_URL, CLIENT_ID, CLIENT_SECRET } from './constants.ts';
