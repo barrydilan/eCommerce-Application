f18292a0ed87b42be315d990256a50c025462343
barrydillan <queharambe@gmail.com>
2023-08-25 16:35:37 +0200

    fix: fix mobile menu item styling

--- src/pages/MenuPage/MenuPage.tsx
+++ src/pages/MenuPage/MenuPage.tsx
@@ -2,7 +2,7 @@ import MenuItem from '../../widgets/MenuItem/MenuItem';
 
 export default function MenuPage() {
   return (
-    <section className="mt-4 flex p-2">
+    <section className="mt-4 flex max-w-[700px] p-2">
       <MenuItem />
     </section>
   );
--- src/widgets/MenuItem/MenuItem.tsx
+++ src/widgets/MenuItem/MenuItem.tsx
@@ -5,18 +5,20 @@ import Rating from '../../features/Rating/Rating';
 export default function MenuItem() {
   return (
     <div className="flex w-full rounded-2xl border-1 border-border-black/10">
-      <div>
-        <img className="mr-2 inline-block" src={productImg} alt={productImg} />
-      </div>
-      <div className="my-4 mr-10 flex flex-col gap-y-1">
-        <h2 className="text-sm font-light text-text-dark">Vegan meal</h2>
-        <h4 className="text-xs font-extralight text-text-grey">622 kcal</h4>
-        <h4 className="text-xs font-extralight text-text-grey">340 g</h4>
-        <div className="hidden">
-          <Rating />
+      <div className="flex flex-[70%] gap-x-2">
+        <div className="flex-[50%]">
+          <img className="mr-2 inline-block" src={productImg} alt={productImg} />
+        </div>
+        <div className="my-4 flex flex-[50%] flex-col gap-y-1">
+          <h2 className="text-sm font-light text-text-dark">Vegan meal</h2>
+          <h4 className="text-xs font-extralight text-text-grey">622 kcal</h4>
+          <h4 className="text-xs font-extralight text-text-grey">340 g</h4>
+          <div className="hidden">
+            <Rating />
+          </div>
         </div>
       </div>
-      <div className="my-4 flex flex-col justify-between">
+      <div className="my-4 flex flex-[30%] flex-col items-end justify-between pr-2">
         <h3 className="text-base font-light text-text-dark">$ 24,50</h3>
         <AddToCartBtn />
       </div>
