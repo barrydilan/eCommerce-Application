c2a0f43c19276b8620bd13a4c3efd5deb4186079
barrydillan <queharambe@gmail.com>
2023-08-28 16:53:03 +0200

    feat: price and rating section

--- src/pages/ProductPage/ui/ProductPage.tsx
+++ src/pages/ProductPage/ui/ProductPage.tsx
@@ -18,11 +18,21 @@ export default function ProductPage() {
           </button>
         </div>
       </div>
-      <div>
-        <div className="px-4 pt-7">
+      <div className="px-4 pt-7">
+        <div className="">
           <h2 className="text-3xl font-bold text-text-dark">Vegan Meal</h2>
           <h3 className="mt-5 text-sm font-light text-text-grey">622 kcal</h3>
           <h3 className="text-sm font-light text-text-grey">340 g</h3>
+        </div>
+        <div className="flex justify-between pt-2">
+          <div>*******</div>
+          <div className="relative">
+            <h2 className="pr-4 text-3xl font-bold text-text-dark">$ 24,25</h2>
+            <span className="absolute -top-5 left-[4.7rem] text-sm font-light text-text-grey line-through">
+              $ 44,50
+            </span>
+            <p className="mt-1 text-sm font-light text-accent">You save: 50%</p>
+          </div>
         </div>
       </div>
     </div>
