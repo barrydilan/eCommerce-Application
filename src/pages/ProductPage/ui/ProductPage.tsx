495fc3408ce7590600e131bb1437f8a1c80150e5
barrydillan <queharambe@gmail.com>
2023-08-28 16:23:26 +0200

    feat: style basic info section

--- src/pages/ProductPage/ui/ProductPage.tsx
+++ src/pages/ProductPage/ui/ProductPage.tsx
@@ -18,6 +18,13 @@ export default function ProductPage() {
           </button>
         </div>
       </div>
+      <div>
+        <div className="px-4 pt-7">
+          <h2 className="text-3xl font-bold text-text-dark">Vegan Meal</h2>
+          <h3 className="mt-5 text-sm font-light text-text-grey">622 kcal</h3>
+          <h3 className="text-sm font-light text-text-grey">340 g</h3>
+        </div>
+      </div>
     </div>
   );
 }
