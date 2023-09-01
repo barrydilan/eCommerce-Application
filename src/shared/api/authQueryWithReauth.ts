f4081a57ec9a0250cd18768685a8cc910aed6371
Bogdan <thegarrybit@gmail.com>
2023-09-02 00:09:22 +0300

    fix: tests

--- src/shared/api/authQueryWithReauth.ts
+++ src/shared/api/authQueryWithReauth.ts
@@ -1,6 +1,52 @@
-import authQuery from './authQuery.ts';
-import { createReauthQuery } from '../lib/helpers';
+import { BaseQueryFn, FetchArgs } from '@reduxjs/toolkit/dist/query/react';
+import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
 
-const authQueryWithReauth = createReauthQuery(authQuery);
+import authQuery from './authQuery.ts';
+import { userSlice } from '../../entities/user';
+import { DEFAULT_CUSTOMER_SCOPE, PROJECT_KEY } from '../const';
+import { ErrorCodeStatus } from '../types';
+
+const authQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
+	args,
+	api,
+	extraOptions,
+) => {
+	const { loggedOut, updateAccessToken } = userSlice.actions;
+	let result = await authQuery(args, api, extraOptions);
+
+	if (result.error && result.error.status === ErrorCodeStatus.UNAUTHORIZED) {
+		const refreshResult = await authQuery(
+			{
+				url: `/oauth/${PROJECT_KEY}/anonymous/token`,
+				method: 'POST',
+				params: {
+					grant_type: 'client_credentials',
+					scope: DEFAULT_CUSTOMER_SCOPE,
+				},
+			},
+			api,
+			extraOptions,
+		);
+
+		if (
+			refreshResult.data &&
+			typeof refreshResult.data === 'object' &&
+			'access_token' in refreshResult.data &&
+			'refresh_token' in refreshResult.data
+		) {
+			api.dispatch(
+				updateAccessToken({
+					accessToken: refreshResult.data.access_token as string,
+					refreshToken: refreshResult.data.refresh_token as string,
+				}),
+			);
+			result = await authQuery(args, api, extraOptions);
+		} else {
+			api.dispatch(loggedOut());
+		}
+	}
+
+	return result;
+};
 
 export default authQueryWithReauth;
