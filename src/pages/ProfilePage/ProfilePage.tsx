bf9c8822ddb2a85658496ac78dd3f05842a35cd3
Tedzury <asdrogachev@gmail.com>
2023-09-01 09:12:29 +0300

    feat: add back button and page header components

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,30 +1,27 @@
-import logOutIcon from '../../assets/icons/log-out.svg';
-import { COOKIE_ACCESS_TOKEN, useAnonymousSessionMutation, userSlice } from '../../entities/user';
-import { COOKIE_USER_ID } from '../../entities/user/consts/constants';
-import deleteCookie from '../../shared/lib/helpers/deleteCookie';
-import { useAppDispatch, useAppSelector } from '../../shared/lib/hooks';
+// import logOutIcon from '../../assets/icons/log-out.svg';
+// import { COOKIE_ACCESS_TOKEN, useAnonymousSessionMutation, userSlice } from '../../entities/user';
+// import { COOKIE_USER_ID } from '../../entities/user/consts/constants';
+// import deleteCookie from '../../shared/lib/helpers/deleteCookie';
+// import { useAppDispatch, useAppSelector } from '../../shared/lib/hooks';
+
+import BackBtn from './model/BackBtn';
+import ProfileHeader from './ui/ProfileHeader';
 
 export default function ProfilePage() {
-  const { isLogged } = useAppSelector((state) => state.userReducer);
-  const dispatch = useAppDispatch();
-  const [getAnonToken, { isLoading }] = useAnonymousSessionMutation();
-  const { loggedOut } = userSlice.actions;
-  async function handleLogout() {
-    const { access_token: accessToken } = await getAnonToken().unwrap();
-    dispatch(loggedOut(accessToken));
-    deleteCookie(COOKIE_ACCESS_TOKEN, COOKIE_USER_ID);
-  }
+  //   const { isLogged } = useAppSelector((state) => state.userReducer);
+  //   const dispatch = useAppDispatch();
+  //   const [getAnonToken, { isLoading }] = useAnonymousSessionMutation();
+  //   const { loggedOut } = userSlice.actions;
+  //   async function handleLogout() {
+  //     const { access_token: accessToken } = await getAnonToken().unwrap();
+  //     dispatch(loggedOut(accessToken));
+  //     deleteCookie(COOKIE_ACCESS_TOKEN, COOKIE_USER_ID);
+  //   }
 
   return (
-    <div className="flex h-1/2 flex-col items-center justify-center">
-      {isLogged && (
-        <div className={`${isLoading ? 'animate-pulse' : ''}`}>
-          <button disabled={isLoading} onClick={handleLogout} type="button" className="navMenuLink text-text-dark">
-            <img src={logOutIcon} alt="" className="navMenuIcon md:inline-block" />
-            Log out
-          </button>
-        </div>
-      )}
+    <div className="p-5">
+      <BackBtn />
+      <ProfileHeader />
     </div>
   );
 }
---
+++ src/pages/ProfilePage/ui/ProfileHeader.tsx
@@ -0,0 +1,8 @@
+export default function ProfileHeader() {
+  return (
+    <div className="mt-6">
+      <h4 className="text-xl font-medium">User information</h4>
+      <p className="mt-1 text-base font-medium text-text-grey">Here you can edit public information about yourself.</p>
+    </div>
+  );
+}
