3609e2255d52f63078bc2de15bb5674e7368ac31
Bogdan <thegarrybit@gmail.com>
2023-09-01 19:16:06 +0300

    implement: handling back

--- src/pages/ProductPage/ui/BackButton.tsx
+++ src/pages/ProductPage/ui/BackButton.tsx
@@ -1,8 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+
 import arrowLeft from '../../../assets/icons/arrowLeft.svg';
 
 function BackButton() {
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate(-1);
+  }
+
   return (
     <button
+      onClick={handleBack}
       className="absolute left-5 top-5 flex h-10 w-10 items-center justify-center rounded-full border-primary/30 backdrop-blur-md sm:h-14 sm:w-14"
       type="button"
     >
