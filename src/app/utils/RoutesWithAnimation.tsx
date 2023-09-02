6d829ed6fae1ab0ab3004e6ca93f1d1fc67e26d9
barrydillan <queharambe@gmail.com>
2023-09-02 23:41:19 +0200

    fix: delete merge markers

--- src/app/utils/RoutesWithAnimation.tsx
+++ src/app/utils/RoutesWithAnimation.tsx
@@ -2,14 +2,8 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 
 import ErrorPage from '../../pages/ErrorPage/ErrorPage';
 import LoginPage from '../../pages/LoginPage/LoginPage';
-<<<<<<< HEAD
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import ProductCatalog from '../../pages/ProductCatalog/ProductCatalog';
-import ProductPage from '../../pages/ProductPage/ui/ProductPage';
-=======
 import ProductCatalogue from '../../pages/ProductCatalog/ProductCatalogue.tsx';
 import ProductPage from '../../pages/ProductPage/ProductPage.tsx';
->>>>>>> 5575ee22d8d0d8eca8e0c1681a142adb8df21616
 import ProfilePage from '../../pages/ProfilePage/ProfilePage';
 import RegPage from '../../pages/RegPage/RegPage';
 import { LoginRequired } from '../../shared/ui';
