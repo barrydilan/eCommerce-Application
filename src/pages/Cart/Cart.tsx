e6d7e2b30c620010e19491b3e492d3c3801c45f0
barrydillan <queharambe@gmail.com>
2023-09-09 00:08:54 +0200

    feat: basic styling of cart item

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,17 +1,30 @@
 export default function Cart() {
   return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae saepe culpa est in non. Laboriosam omnis
-      doloribus adipisci quas, exercitationem, accusamus nesciunt tenetur quis tempore nobis assumenda vitae odit
-      tempora? Ducimus vero, qui aperiam sed eius tempora quia quos architecto, quae reiciendis praesentium, quaerat
-      quidem nobis! Ipsa labore accusantium, dolor beatae deleniti commodi nesciunt nobis alias cupiditate, consectetur
-      quam quod! Fugit dignissimos quos voluptas et omnis, ab cupiditate libero consequuntur ipsam quia dolorum
-      excepturi iure illo molestias eos quasi, deleniti eum suscipit voluptate, quas vitae quam! Recusandae amet
-      asperiores magnam. Nemo, labore soluta. Mollitia quae nisi at tempora, saepe architecto repudiandae repellat
-      omnis! Nemo, eligendi molestias incidunt iure ex aperiam, molestiae accusamus quibusdam nostrum, voluptatum
-      recusandae saepe quasi laborum corrupti. Nesciunt, in eaque hic tempora molestiae, dolorum cumque incidunt alias
-      excepturi fuga error repudiandae sit minima totam maiores temporibus harum beatae omnis! Reprehenderit corrupti
-      enim accusantium soluta accusamus dolorem nisi.
+    <div
+      className="
+    mt-14
+    px-[10px]
+    sm:mt-16
+    md:px-[6px]
+    md:py-[6px]
+"
+    >
+      <h2 className="mb-6 mt-12 text-xl">Your order</h2>
+      <div className="relative flex items-center justify-between gap-x-1 border-b-1 border-text-grey/40 pb-4">
+        <img src="src/assets/img/cart-img.png" alt="" />
+        <div className="flex flex-col gap-y-4">
+          <h3 className="text-xs">Vegan Meal</h3>
+          <div className="flex items-center gap-x-2 pl-2">
+            <div className="text-xs">-</div>
+            <div className="text-xs">1</div>
+            <div className="text-xs">+</div>
+          </div>
+        </div>
+        <div className="block self-end pb-1">
+          <span className="block self-end text-xs">$24,50</span>
+        </div>
+        <div className="absolute right-0 top-0">X</div>
+      </div>
     </div>
   );
 }
