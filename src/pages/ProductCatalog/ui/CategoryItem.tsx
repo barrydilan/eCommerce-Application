438e0c37cb299c79e592e371f7aea59cc556d3c4
Bogdan <thegarrybit@gmail.com>
2023-09-08 18:52:04 +0300

    feat: add categories animation

--- src/pages/ProductCatalog/ui/CategoryItem.tsx
+++ src/pages/ProductCatalog/ui/CategoryItem.tsx
@@ -1,13 +1,19 @@
+import React from 'react';
+
+import { motion } from 'framer-motion';
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 
 interface ICategoryItemProps {
   item: string;
   activeCat: string;
+  // eslint-disable-next-line react/require-default-props
+  children?: React.ReactNode | null;
+  index: number;
 }
 
-const greenBorder = 'border-b-2 border-accent';
+// const greenBorder = 'border-b-2 border-accent';
 
-function CategoryItem({ item, activeCat }: ICategoryItemProps) {
+function CategoryItem({ item, activeCat, index, children = null }: ICategoryItemProps) {
   const [query] = useSearchParams();
   const navigate = useNavigate();
   const { pathname } = useLocation();
@@ -35,16 +41,28 @@ function CategoryItem({ item, activeCat }: ICategoryItemProps) {
   }
 
   return (
-    <li className={`whitespace-nowrap px-1 ${isActive ? greenBorder : ''}`}>
+    <motion.li
+      initial={{ y: isPrevCategory ? '0' : '100%' }}
+      animate={{ y: 0 }}
+      transition={{
+        type: 'spring',
+        stiffness: 660,
+        damping: 23,
+        delay: 0.03 * index,
+      }}
+      className="flex items-center gap-3 whitespace-nowrap px-1"
+    >
       <button
-        className={`${isActive ? 'text-text-dark' : ''}`}
+        className={`${isActive ? 'text-text-dark' : ''} relative`}
         onClick={isPrevCategory ? goBack : goForward}
         data-user-select={item}
         type="button"
       >
         {item}
+        {isActive ? <span className="absolute -bottom-1 left-0 h-0.5 w-full bg-accent" /> : null}
       </button>
-    </li>
+      {children}
+    </motion.li>
   );
 }
 
