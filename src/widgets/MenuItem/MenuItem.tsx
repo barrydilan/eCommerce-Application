76e21403fbf86534e99336a0b75694a62c3a1093
Bogdan <thegarrybit@gmail.com>
2023-09-14 11:54:00 +0300

    fix: catalogue items width

--- src/widgets/MenuItem/MenuItem.tsx
+++ src/widgets/MenuItem/MenuItem.tsx
@@ -37,14 +37,14 @@ export default function MenuItem({ name, image, id, attributes, prices, isSpicy,
   return (
     <li className="w-full list-none">
       <Link to={`/product/${id}`}>
-        <div className="flex max-h-[170px] rounded-2xl border-1 border-border-black/10 pr-4 transition-all ease-in-out hover:bg-accent-light dark:border-dark-separation-line dark:hover:bg-dark-separation-line md:min-h-[170px] md:pr-8">
+        <div className="flex h-[170px] rounded-2xl border-1 border-border-black/10 pr-4 transition-all ease-in-out hover:bg-accent-light dark:border-dark-separation-line dark:hover:bg-dark-separation-line md:pr-8">
           <div className="relative flex flex-[75%] gap-x-4 sm:gap-x-7">
             <img
               onLoad={() => setIsLoading(false)}
               loading="lazy"
               className={`${
                 isLoading ? 'h-full w-48 opacity-0' : 'opacity-100'
-              } inline-block h-full max-w-[40%] flex-none rounded-2xl object-cover transition-all duration-300 ease-bounce xs:max-w-[30%] md:max-w-[25%] xl:max-w-[30%]`}
+              } inline-block h-full max-w-[50%] flex-none rounded-2xl object-cover transition-all duration-300 ease-bounce md:w-[160px]`}
               src={image}
               alt={name}
             />
@@ -55,10 +55,10 @@ export default function MenuItem({ name, image, id, attributes, prices, isSpicy,
             ) : null}
             <div className="flex flex-col justify-center gap-y-3 py-4 md:py-7">
               <div className="flex flex-col gap-y-3">
-                <div className="truncate-text mr-3 text-text-dark dark:text-primary sm:text-base lg:mr-7">{name}</div>
+                <div className="truncate-text mr-3 text-lg text-text-dark dark:text-primary lg:mr-7">{name}</div>
                 <div className="grid gap-1">
-                  <h4 className="text-xs text-text-grey">{calories} kcal</h4>
-                  <h4 className="text-xs text-text-grey">{weight} g</h4>
+                  <h4 className="text-text-grey">{calories} kcal</h4>
+                  <h4 className="text-text-grey">{weight} g</h4>
                 </div>
               </div>
               <div className="flex max-w-[40px] items-center gap-x-2">
@@ -73,11 +73,11 @@ export default function MenuItem({ name, image, id, attributes, prices, isSpicy,
               </div>
             </div>
           </div>
-          <div className="mt-2 flex flex-col items-end justify-end py-4 md:py-7">
+          <div className="flex flex-col items-end justify-end py-4 md:py-7">
             {oldPrice ? (
               <span className="justify-self-end text-sm text-text-grey line-through md:text-base">$ {oldPrice}</span>
             ) : null}
-            <h3 className="mt-1 text-base font-semibold text-text-dark dark:text-primary lg:text-lg">$ {corePrice}</h3>
+            <h3 className="mt-1 text-lg font-semibold text-text-dark dark:text-primary lg:text-lg">$ {corePrice}</h3>
             <AddToCartBtn />
           </div>
         </div>
