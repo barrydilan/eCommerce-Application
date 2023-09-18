cb038c30c5ba20df2771c0c4d7e4e6974af8ee7a
barrydillan <queharambe@gmail.com>
2023-09-18 20:01:34 +0200

    feat: add animation to promo

--- src/pages/ProductCatalog/ui/PromoBanner.tsx
+++ src/pages/ProductCatalog/ui/PromoBanner.tsx
@@ -1,21 +1,54 @@
+import { useState } from 'react';
+
+import { AnimatePresence, motion } from 'framer-motion';
+
 export default function PromoBanner() {
+  const [showMessage, setShowMessage] = useState(false);
+
   const PROMOCODE = 'STEALDEAL20';
+
+  const handleButtonClick = () => {
+    setShowMessage(true);
+    setTimeout(() => {
+      setShowMessage(false);
+    }, 1000);
+  };
+
   return (
     <div className="mx-auto mt-4 flex w-full">
       <div className="relative self-center overflow-hidden rounded-lg bg-gradient-to-br from-shop-cart-red to-rating-star  py-4 text-center text-white shadow-md">
-        <h3 className="mb-4 px-12 text-2xl font-semibold">20% flat off on all items only today!</h3>
+        <h3 className="mb-4 px-12 text-2xl font-semibold">15% flat off on all items only today!</h3>
         <div className="mx-auto flex justify-center space-x-2 first-line:mb-6 xs:w-[60%]">
-          <span
+          <motion.span
             id="cpnCode"
-            className="flex items-center justify-center rounded-l border border-dashed px-4 py-2 text-white"
+            className="relative flex items-center justify-center rounded-l border border-dashed px-4 py-2 text-white"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
           >
-            {PROMOCODE}
-          </span>
+            {showMessage ? (
+              <AnimatePresence>
+                <motion.div
+                  className="opacity-90"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                >
+                  Text Copied!
+                </motion.div>
+              </AnimatePresence>
+            ) : (
+              'myLovelyPromoCode'
+            )}
+          </motion.span>
           <button
             type="button"
             id="cpnBtn"
             className="focus:scale-80 flex transform animate-wiggle cursor-pointer items-center justify-center rounded-sm border border-white bg-white px-2 py-2 text-sm text-shop-cart-red shadow-lg outline-none transition-transform active:scale-75"
-            onClick={() => navigator.clipboard.writeText(PROMOCODE)}
+            onClick={() => {
+              handleButtonClick();
+              navigator.clipboard.writeText(PROMOCODE);
+            }}
           >
             Copy Code
           </button>
