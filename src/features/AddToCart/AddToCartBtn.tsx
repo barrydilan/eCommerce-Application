a4545dd4f520cc80a5c14584f8314150c82e35e5
Tedzury <asdrogachev@gmail.com>
2023-09-19 08:32:32 +0300

    feat: add logic to product cart for adding products to cart and disable after it

--- src/features/AddToCart/AddToCartBtn.tsx
+++ src/features/AddToCart/AddToCartBtn.tsx
@@ -1,12 +1,21 @@
 import { useGetCartByIdQuery, useUpdateCartMutation } from '../../entities/cart';
 import { AddLineItemRequestBody } from '../../entities/cart/types/types';
 import { useAppSelector } from '../../shared/lib/hooks';
+import { LineItem } from '../../shared/types';
 
 export default function AddToCartBtn(props: { productId: string }) {
   const { cartId } = useAppSelector((state) => state.userReducer);
   const { data: cart } = useGetCartByIdQuery(cartId);
-  const [updateLineItem] = useUpdateCartMutation();
+  const [updateLineItem, { isLoading: updateIsLoading }] = useUpdateCartMutation();
   const { productId } = props;
+
+  if (!cart) return null;
+
+  const { lineItems } = cart;
+  const currItem = lineItems?.find((item) => item?.productId === productId) as LineItem;
+  const quantity = currItem?.quantity ?? 0;
+
+  const disabled = quantity > 0;
 
   const addToCart = async () => {
     const body: AddLineItemRequestBody = {
@@ -31,11 +40,15 @@ export default function AddToCartBtn(props: { productId: string }) {
   return (
     <button
       type="button"
+      disabled={disabled}
       onClick={(e) => {
+        e.preventDefault();
         e.nativeEvent.stopImmediatePropagation();
         addToCart();
       }}
-      className="mt-auto inline-block min-w-[30px] max-w-[50px] self-end rounded-md bg-accent-lightest text-2xl text-accent transition delay-150 duration-300 ease-in-out hover:bg-accent hover:text-accent-light"
+      className={`${
+        updateIsLoading ? 'cursor-wait opacity-30' : ''
+      } mt-auto inline-block min-w-[30px] max-w-[50px] self-end rounded-md bg-accent-lightest text-2xl text-accent transition delay-150 duration-300 ease-in-out hover:bg-accent hover:text-accent-light disabled:bg-separation-line disabled:text-text-grey`}
     >
       +
     </button>
